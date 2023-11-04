ef9970899bb431334f4350171fd6d72f04f66013
Yasiel <yasielglez90210@gmail.com>
2023-11-04 11:39:27 -0400

    utilizando useReducer

--- shopping-cart/src/context/cart.jsx
+++ shopping-cart/src/context/cart.jsx
@@ -1,46 +1,63 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react'
+import { cartReducer, cartInitialState, CART_ACTIONS } from '../reducers/cart'
 
 export const CartContext = createContext()
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([])
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
 
   const addToCart = (product) => {
-    const productIndex = cart.findIndex((item) => item.id === product.id)
-
-    if (productIndex >= 0) {
-      const newCart = structuredClone(cart)
-      newCart[productIndex].quantity += 1
-      return setCart(newCart)
-    }
-
-    setCart((prevState) => [...prevState, { ...product, quantity: 1 }])
+    dispatch({ type: CART_ACTIONS.ADD_TO_CART, payload: product })
   }
 
-  const removeFromCart = (productId) => {
-    const productIndex = cart.findIndex((item) => item.id === productId)
-
-    if (productIndex >= 0) {
-      if (cart[productIndex].quantity > 1) {
-        const newCart = structuredClone(cart)
-        newCart[productIndex].quantity -= 1
-        return setCart(newCart)
-      } else {
-        const newCart = structuredClone(cart)
-        newCart.splice(productIndex, 1)
-        return setCart(newCart)
-      }
-    }
+  const removeFromCart = (id) => {
+    dispatch({ type: CART_ACTIONS.REMOVE_FROM_CART, payload: { id } })
   }
 
   const cleanCart = () => {
-    setCart([])
+    dispatch({ type: CART_ACTIONS.CLEAN_CART })
   }
+
+  // Usando UseState
+
+  // const [cart, setCart] = useState([])
+
+  // const addToCart = (product) => {
+  //   const productIndex = cart.findIndex((item) => item.id === product.id)
+
+  //   if (productIndex >= 0) {
+  //     const newCart = structuredClone(cart)
+  //     newCart[productIndex].quantity += 1
+  //     return setCart(newCart)
+  //   }
+
+  //   setCart((prevState) => [...prevState, { ...product, quantity: 1 }])
+  // }
+
+  // const removeFromCart = (productId) => {
+  //   const productIndex = cart.findIndex((item) => item.id === productId)
+
+  //   if (productIndex >= 0) {
+  //     if (cart[productIndex].quantity > 1) {
+  //       const newCart = structuredClone(cart)
+  //       newCart[productIndex].quantity -= 1
+  //       return setCart(newCart)
+  //     } else {
+  //       const newCart = structuredClone(cart)
+  //       newCart.splice(productIndex, 1)
+  //       return setCart(newCart)
+  //     }
+  //   }
+  // }
+
+  // const cleanCart = () => {
+  //   setCart([])
+  // }
 
   return (
     <CartContext.Provider
       value={{
-        cart,
+        cart: state,
         addToCart,
         removeFromCart,
         cleanCart,
---
+++ shopping-cart/src/reducers/cart.jsx
@@ -0,0 +1,49 @@
+export const cartInitialState = []
+
+export const CART_ACTIONS = {
+  ADD_TO_CART: 'ADD_TO_CART',
+  REMOVE_FROM_CART: 'REMOVE_FROM_CART',
+  CLEAN_CART: 'CLEAN_CART',
+}
+
+export const cartReducer = (state, action) => {
+  const { type: actionType, payload: actionPayload } = action
+
+  switch (actionType) {
+    case CART_ACTIONS.ADD_TO_CART: {
+      const { id } = actionPayload
+      const productIndex = state.findIndex((item) => item.id === id)
+
+      if (productIndex >= 0) {
+        const newState = structuredClone(state)
+        newState[productIndex].quantity += 1
+        return newState
+      }
+
+      return [...state, { ...actionPayload, quantity: 1 }]
+    }
+
+    case CART_ACTIONS.REMOVE_FROM_CART: {
+      const { id } = actionPayload
+      const productIndex = state.findIndex((item) => item.id === id)
+
+      if (productIndex >= 0) {
+        if (state[productIndex].quantity > 1) {
+          const newState = structuredClone(state)
+          newState[productIndex].quantity -= 1
+          return newState
+        } else {
+          const newState = structuredClone(state)
+          newState.splice(productIndex, 1)
+          return newState
+        }
+      }
+    }
+
+    case CART_ACTIONS.CLEAN_CART: {
+      return cartInitialState
+    }
+  }
+
+  return state
+}
