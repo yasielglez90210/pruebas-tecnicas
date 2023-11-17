4056a1f74ad54f78776e51f6c0f4ad7fbe81b848
Yasiel <yasielglez90210@gmail.com>
2023-11-17 00:37:59 -0500

    estado global con zustand, proceso de seleccion de respuesta

--- javascript-quiz/.eslintrc.cjs
+++ javascript-quiz/.eslintrc.cjs
@@ -5,14 +5,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
   },
 }
