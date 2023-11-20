e747a55a0fceb5cbd0846d558c2e3367f28f1492
Yasiel <yasielglez90210@gmail.com>
2023-11-20 17:41:06 -0500

    subiendo app para buscar peliculas

---
+++ search-movies/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    './node_modules/standard/eslintrc.json',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/jsx-no-target-blank': 'off',
+    'jsx-quotes': 'off',
+    'space-before-function-paren': 'off',
+  },
+}
---
+++ search-movies/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Buscador de películas</h1>
+    </>
+  )
+}
+
+export default App
