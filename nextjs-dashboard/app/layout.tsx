9e3a3169595f7bfb8434c018719c205c5c61552c
Yasiel <yasielglez90210@gmail.com>
2023-12-12 13:29:57 -0500

    subiendo app base nextjs-dashboard

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { montserrat } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
